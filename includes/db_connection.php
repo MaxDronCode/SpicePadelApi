43ab163690e700243aad912ce393ce2b29c1e1a7
Max <Max>
2024-05-23 10:31:34 +0200

    create remote db

--- includes/db_connection.php
+++ includes/db_connection.php
@@ -1,8 +1,8 @@
 <?php
-    $servername = "localhost";
-    $database = "test";
-    $username = "root";
-    $password = "";
+    $servername = "sql8.freemysqlhosting.net";
+    $database = "sql8708583";
+    $username = "sql8708583";
+    $password = "S5D7QZwMxv";
     $port = 3306;
     // Create connection
     $conn = mysqli_connect($servername, $username, $password, $database, $port);
@@ -11,4 +11,10 @@
     if (!$conn) {
         die("Connection failed: " . mysqli_connect_error());
     }
+
+    // server : sql8.freemysqlhosting.net
+    // db_name : sql8708583
+    // user: sql8708583
+    // pass : S5D7QZwMxv
+    // port : 3306
 ?>
