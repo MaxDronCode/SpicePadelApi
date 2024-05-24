867b0c85fd480d18679d230a6fd8090fac88d60f
Max <Max>
2024-05-24 21:21:50 +0200

    se modifica getUsers, una api que muestra los posibles usuarios para emparejarse. Ahora solo devuelve gente sin pareja

--- api/getUsers.php
+++ api/getUsers.php
@@ -1,21 +1,20 @@
 <?php
-    // Permetre solicituds creuades
     header("Access-Control-Allow-Origin: *");
     header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
     header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
-    require_once"../includes/db_connection.php";
+    require_once "../includes/db_connection.php";
 
     if ($_SERVER['REQUEST_METHOD'] == 'GET') {
-        $q = "SELECT email FROM user u JOIN member m WHERE u.dni = m.dni_m";
+        $q = "SELECT u.email FROM user u 
+              JOIN member m ON u.dni = m.dni_m
+              WHERE NOT EXISTS (SELECT 1 FROM team t WHERE t.player1_dni = m.dni_m OR t.player2_dni = m.dni_m)";
+
         $result = mysqli_query($conn, $q);
-
         $users = array();
-
         while ($row = mysqli_fetch_assoc($result)) {
             $users[] = $row;
         }
-
         echo json_encode($users);
     }
 ?>
