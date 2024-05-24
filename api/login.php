67fd77e986089dae84c5f216c767c32a2a5c4dca
Max <Max>
2024-05-24 18:14:08 +0200

    modificacion de login para que checkee si el user que entra es un admin o no

--- api/login.php
+++ api/login.php
@@ -1,10 +1,10 @@
 <?php
-    // Permetre solicituds creuades
+    // Permitir solicitudes cruzadas
     header("Access-Control-Allow-Origin: *");
     header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
     header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
-    require_once"../includes/db_connection.php";
+    require_once "../includes/db_connection.php";
 
     if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         $data = json_decode(file_get_contents('php://input'), true);
@@ -17,23 +17,32 @@
         $arr_result = mysqli_fetch_array($result);
 
         if ($arr_result[0] == 1) {
-            // coger el mail porque nos interesa devolverlo al front para mostrarlo
+            // Obtener el email del usuario
             $q = "SELECT email FROM user WHERE dni = '$dni' AND password = '$password'";
             $result = mysqli_query($conn, $q);
             $arr_result = mysqli_fetch_array($result);
             $user_mail = $arr_result['email']; // tenemos el mail para devolver al front
 
+            // Comprobar si el usuario es un administrador
+            $q = "SELECT EXISTS (SELECT 1 FROM admin WHERE dni_a = '$dni')";
+            $result = mysqli_query($conn, $q);
+            $isAdmin = mysqli_fetch_array($result)[0] == 1; // true si es admin
 
-            // generar el token y devolverlo
+            // Generar el token y devolverlo
             $token = bin2hex(random_bytes(16));
             header('Content-Type: application/json');
-            echo json_encode(['success' => true, 'message' => 'Login successful', 'token' => $token, 'user_mail' => $user_mail]);
+            echo json_encode([
+                'success' => true, 
+                'message' => 'Login successful', 
+                'token' => $token, 
+                'user_mail' => $user_mail, 
+                'admin' => $isAdmin
+            ]);
         } else {
             header('Content-Type: application/json');
             echo json_encode(['success' => false, 'message' => 'User not found']);
         }
 
         $conn->close();
-        
     }
 ?>
