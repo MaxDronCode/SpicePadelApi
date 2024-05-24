a062b4358cb10140a46d5be373e8356d3709fa46
Max <Max>
2024-05-24 18:56:07 +0200

    admin puede ver los users

---
+++ api/getAllUsers.php
@@ -0,0 +1,25 @@
+<?php
+    // Permetre solicituds creuades
+    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+    header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
+
+
+    require_once"./includes/db_connection.php";
+
+    // obtenir tots els users
+     
+    if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+    $result = mysqli_query($conn, "SELECT * FROM user");
+    $categories = array();
+    while ($row = mysqli_fetch_assoc($result)) {
+        $categories[] = $row;
+    }
+    header('Content-Type: application/json');
+    echo json_encode($categories);
+    }
+
+
+
+?>
