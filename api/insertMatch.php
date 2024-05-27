34c93a3fda3dd7b0d3b61d446f9bc38aa5bf3fbc
Max <Max>
2024-05-27 20:24:07 +0200

    mejoras en insert match, ahora lo hace en pistas diferentes y horas diferetnes

--- api/insertMatch.php
+++ api/insertMatch.php
@@ -1,10 +1,9 @@
 <?php
-    // Permetre solicituds creuades
     header("Access-Control-Allow-Origin: *");
     header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
     header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
-    require_once"../includes/db_connection.php";
+    require_once "../includes/db_connection.php";
 
     if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         $data = json_decode(file_get_contents('php://input'), true);
@@ -12,7 +11,37 @@
         $team1_id = $data['team1_id'];
         $team2_id = $data['team2_id'];
 
-        $q = "INSERT INTO `match` VALUES (DEFAULT, '$team1_id', '$team2_id', '0', '0', '10:00', '1')";
+        // Obtener pistas disponibles
+        $queryFields = "SELECT id FROM field WHERE status = 'available'";
+        $resultFields = mysqli_query($conn, $queryFields);
+        $availableFields = [];
+        while ($row = mysqli_fetch_assoc($resultFields)) {
+            $availableFields[] = $row['id'];
+        }
+
+        // Seleccionar una pista aleatoria
+        $randomFieldIndex = array_rand($availableFields);
+        $selectedFieldId = $availableFields[$randomFieldIndex];
+
+        // Calcular la hora del próximo partido
+        $baseTime = new DateTime('10:00');
+        $queryTime = "SELECT COUNT(id) as num_matches FROM `match` WHERE match_time >= '10:00' AND match_time < '18:00'";
+        $resultTime = mysqli_query($conn, $queryTime);
+        $dataTime = mysqli_fetch_assoc($resultTime);
+        $num_matches = $dataTime['num_matches'];
+
+        // Calculamos el intervalo de tiempo
+        $minutesToAdd = $num_matches * 30;
+        $baseTime->modify("+$minutesToAdd minutes");
+        $match_time = $baseTime->format('H:i');
+
+        // Resetear a las 10:00 si es mayor o igual a las 18:00
+        if ($match_time >= '18:00') {
+            $match_time = '10:00';
+        }
+
+        // Insertar el partido
+        $q = "INSERT INTO `match` VALUES (DEFAULT, '$team1_id', '$team2_id', '0', '0', '$match_time', '$selectedFieldId')";
         mysqli_query($conn, $q);
 
         $q = "SELECT max(id) as last_match_id FROM `match`";
@@ -21,8 +50,7 @@
         $last_match_id = $arr_result['last_match_id'];
 
         $q = "INSERT INTO match_teams VALUES ('$team1_id', '$team2_id', '$last_match_id')";
-
-        if (mysqli_query($conn, $q)){
+        if (mysqli_query($conn, $q)) {
             header('Content-Type: application/json');
             echo json_encode(['success' => true, 'message' => 'Insert match ok']);
         } else {
@@ -31,6 +59,5 @@
         }
         
         $conn->close();
-        
     }
 ?>
