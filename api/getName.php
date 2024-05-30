07daf5da516197a94dbe596bfd8baa7fdb58f506
Max <Max>
2024-05-30 12:10:17 +0200

    getname

--- api/getName.php
+++ api/getName.php
@@ -7,12 +7,10 @@
     require_once"../includes/db_connection.php";
 
     if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-
         $data = json_decode(file_get_contents('php://input'), true);
 
         $user_email = $data['user_email'];
-
-        $q= "SELECT CONCAT(name, ' ', surname1) AS player1_name, dni, birthday from user join member on dni = dni_m where email='$user_email'";
+        $q= "SELECT CONCAT(name, ' ', surname1) AS player1_name, dni, birthday FROM user JOIN member ON dni = dni_m WHERE email='$user_email'";
         $result = mysqli_query($conn, $q);
         $arr_result = mysqli_fetch_assoc($result);
         $output = ['user_name' => $arr_result['player1_name'], 'dni' => $arr_result['dni'], 'birthday' => $arr_result['birthday']];
